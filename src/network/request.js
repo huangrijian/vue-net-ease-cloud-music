56a4b642663adcf87c03428e7b4c615a8a1b8fa1
黄日健 <923691372@qq.com>
2021-04-03 23:38:12 +0800

    更新

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: 'http://bao.lqjhome.cn:3000',
+    baseURL: 'http://localhost:3000',
     timeout: 5000
   })
 
